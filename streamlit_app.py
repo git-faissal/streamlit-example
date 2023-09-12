2fd0c211fc7a4cd8fa55f780e8d348919601729c
git-faissal <100127255+git-faissal@users.noreply.github.com>
2023-09-12 11:15:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,8 @@ def extract_text_from_pdf(file_path):
 #Fonction appel aux modele de resume de texte en utilisant l'appel de l'API
 
 def query(payload):
-    API_URL = "https://api-inference.huggingface.co/models/tuner007/pegasus_summarizer"
+    #API_URL = "https://api-inference.huggingface.co/models/tuner007/pegasus_summarizer"
+    API_URL = "https://api-inference.huggingface.co/models/gpt2"
     headers = {"Authorization": "Bearer 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"}
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
