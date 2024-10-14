69f55edd01c01565333a07a90ef05a5c7ba339ad
git-faissal <100127255+git-faissal@users.noreply.github.com>
2024-10-14 12:36:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ choice = st.sidebar.selectbox(
 def run_app():
     if choice == "Resumer un Texte":
         st.subheader("NB: Dans cette zone, vous pourrez tester le modèle pour résumer vos reportages effectués sur le terrain. Ainsi, vous pourrez permettre à vos lecteurs de lire vos résumés et de se renseigner plus rapidement.")
-         st.subheader("Ceci est un prototype de test en cours de développement. !!!")
+        st.subheader("Ceci est un prototype de test en cours de développement. !!!")
         st.subheader("Resume de texte avec Pegasus_Summarizer")
         input_text = st.text_area("Entrez votre texte ici")
         if st.button("Texte Resume"):
